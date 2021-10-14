a003928dda3c7ce53c8aaf12890b80255fc8567f
Wali <walikhan226@gmail.com>
2021-10-14 22:53:40 +0500

    native code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,23 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    
+    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+    let CHANNEL = FlutterMethodChannel(name:"flutter.native/helper",binaryMessenger:controller as! FlutterBinaryMessenger)
+    
+    CHANNEL.setMethodCallHandler {
+        
+        [unowned self] (methodCall, result ) in
+        
+        if methodCall.method == "helloFromNativeCode"
+        {
+            
+            result("hello from swfit")
+        }
+        
+    }
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 void main() {
@@ -7,7 +7,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  // const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
